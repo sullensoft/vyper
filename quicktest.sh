24309bd8df05b93abcd162f5fa4681378c838aab
sullen-software <102875839+sullen-software@users.noreply.github.com>
2022-04-03 17:36:52 -0400

    Delete quicktest.sh

--- quicktest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# examples:
-# ./quicktest.sh
-# ./quicktest.sh tests/.../mytest.py
-
-# run pytest but bail out on first error and suppress coverage.
-# useful for dev workflow
-pytest -q --no-cov -s --instafail -x --disable-warnings "$@"
